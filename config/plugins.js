5dc3dfa082476cd0c98ec42e885297ec1908f4da
Saood Ahmad <saoodchoudhary9@gmail.com>
2024-12-28 10:08:34 +0530

    iup

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'local',
+    providerOptions: {
+      path: path.join('/data/uploads'), // Persistent disk path
+      sizeLimit: 1000000, // Adjust size limit if needed
+    },
+  },
+});
